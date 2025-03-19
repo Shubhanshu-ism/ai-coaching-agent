36184eed4311e2f4eedd4520d1cc5d7f334b6b8e
skj <shubhanshujain2233@gmail.com>
2025-03-20 05:09:14 +0530

    save-user-data

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,15 +1,16 @@
 'use client'
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import React from 'react'
+import React, { Suspense } from 'react'
+import AuthProvider from "./AuthProvider";
 
 function Provider({children}) {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
   return (
-    <div>
-        <ConvexProvider client={convex}>
-            {children}
-        </ConvexProvider>
-    </div>
+    <Suspense fallback={<p>Loading....</p>}>
+      <ConvexProvider client={convex}>
+        <AuthProvider>{children}</AuthProvider>
+      </ConvexProvider>
+    </Suspense>
   );
 }
 
--- convex/schema.js
+++ convex/schema.js
@@ -2,10 +2,10 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    users: defineTable({
-        name: v.string(),
-        email: v.string(),
-        credits: v.number(),
-        subcriptionId: v.string()
-    })
-})
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+    credits: v.number(),
+    subcriptionId: v.optional(v.string()),
+  }),
+});
