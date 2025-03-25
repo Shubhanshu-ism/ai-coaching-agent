04a58a71a95ae6a84a0c831850bac8dc97b248a1
skj <shubhanshujain2233@gmail.com>
2025-03-25 21:01:58 +0530

    dashboard redirect

--- app/(main)/_component/AppHeader.jsx
+++ app/(main)/_component/AppHeader.jsx
@@ -1,14 +1,23 @@
-import { UserButton } from '@stackframe/stack'
-import Image from 'next/image'
-import React from 'react'
+import { UserButton } from "@stackframe/stack";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function AppHeader() {
   return (
     <div className="p-3 shadow-sm flex justify-between items-center">
-      <Image src={"/geminiCoachingLogo.svg"} alt="logo" width={150} height={100}  />
+      <Link href="/dashboard">
+        <Image
+          src={"/geminiCoachingLogo.svg"}
+          alt="logo"
+          width={150}
+          height={100}
+          className="cursor-pointer"
+        />
+      </Link>
       <UserButton />
     </div>
   );
 }
 
-export default AppHeader
+export default AppHeader;
